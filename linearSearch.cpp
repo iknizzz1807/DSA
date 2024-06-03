b5f63ca24244eeeff823f4b85260fa66473c56f1
Nguyen My Thong <90298623+iknizzz1807@users.noreply.github.com>
2024-06-04 01:13:50 +0700

    Add linearSearch

---
+++ linearSearch.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int linearSearch(const int arr[], int n, int x) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == x) return i;
+    }
+    return -1;
+}
+
+int main() {
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    // linearSearch
+    int x; cin >> x;
+    cout << linearSearch(arr, n, x);
+    return 0;
+}
+
